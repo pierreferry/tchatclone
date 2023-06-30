b4887761dec27a7685b02da64cea5845643d07cb
Pierre Ferry <pierre.ferry@preligens.com>
2023-06-30 16:35:12 +0200

    ✨Improve chat page
    The user can now see mocked messages and post new messages.
    TODO : connect this to a real backend

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,7 +1,41 @@
-function Chat() {
-  return <div className="Chat">
-    <h4>Welcome to the chat room!</h4>
-  </div>
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+
+const mockMessages = [
+  { id: 1, username: "John", text: "Hello!" },
+  { id: 2, username: "Jane", text: "Hi!" },
+  { id: 3, username: "John", text: "How are you?" },
+];
+
+function Chat({ user }) {
+  const [messages, setMessages] = useState(mockMessages);
+  const { register, handleSubmit, setValue } = useForm();
+
+  return (
+    <div className="Chat">
+      <h4>Welcome to the chat room!</h4>
+      {messages.map((message) => (
+        <div key={message.id} className="Chat-message">
+          <span className="Chat-message-username">{message.username}: </span>
+          <span>{message.text}</span>
+        </div>
+      ))}
+      <form
+        className="Chat-form"
+        onSubmit={handleSubmit((data) => {
+          console.log(data);
+          setMessages((oldMessages) => [
+            ...oldMessages,
+            { id: oldMessages.length + 1, username: user, text: data.message },
+          ]);
+          setValue("message", "");
+        })}
+      >
+        <input type="text" {...register("message")} />
+        <button type="submit">Chat</button>
+      </form>
+    </div>
+  );
 }
 
 export default Chat;
