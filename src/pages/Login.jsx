bc78c4d7c3f42f1c5a3a930a183c291a937b33b1
Pierre Ferry <pierre.ferry@preligens.com>
2023-06-30 15:21:33 +0200

    ✨Improve login page
    The user can now select a username.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,26 @@
+import { useForm } from "react-hook-form";
+import "./Login.css";
+
 function Login({ setUser }) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
   return (
-    <div className="Login" onClick={() => setUser("User")}>Login</div>
+    <form
+      className="Login"
+      onSubmit={handleSubmit((formData) => setUser(formData.username))}
+    >
+      <label htmlFor="username">Username</label>
+      <input id="username" {...register("username", { required: true })} />
+      {errors.username && (
+        <span>Please select a username before joining the chat.</span>
+      )}
+
+      <button type="submit">Enter chat</button>
+    </form>
   );
 }
 
