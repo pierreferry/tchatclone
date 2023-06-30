d6395ba16021d523aa61710618d0c261e75728a4
Pierre Ferry <pierre.ferry@preligens.com>
2023-07-01 00:12:56 +0200

    ✨ Create messages and new-messages endpoints

--- back/src/index.js
+++ back/src/index.js
@@ -1,16 +1,30 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const mockMessages = [
+  { id: 1, username: "John", text: "Hello!" },
+  { id: 2, username: "Jane", text: "Hi!" },
+  { id: 3, username: "John", text: "How are you?" },
+];
 
 app.get("/messages", (req, res) => {
-  res.json(["Hello", "World", "!"
-  ]);
-})
+  res.json(mockMessages);
+});
+
+app.post("/new-message", (req, res) => {
+  const { username, text } = req.body;
+  const newMessage = { id: mockMessages.length + 1, username, text };
+  mockMessages.push(newMessage);
+  res.json(newMessage);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
